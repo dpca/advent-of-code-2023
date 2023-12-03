0786537a26c6b1309e338ee41843ddd745c30c21
Dan Cahoon <dpcahoon@gmail.com>
2023-12-03 12:00:54 -0500

    Comments

--- src/day3.rs
+++ src/day3.rs
@@ -2,16 +2,7 @@ use std::fs;
 
 // Common
 
-fn parse_input() -> Vec<Vec<char>> {
-    let contents = fs::read_to_string("./inputs/day3.txt").expect("No file found");
-    let lines = contents.split("\n");
-
-    return lines
-        .filter(|l| l.len() > 0)
-        .map(|l| l.chars().collect())
-        .collect();
-}
-
+// Used to go to each coordinate up, down, left, right, and diagonals
 const CHECK_COORDS: [(i32, i32); 8] = [
     (-1, -1),
     (-1, 0),
@@ -23,29 +14,44 @@ const CHECK_COORDS: [(i32, i32); 8] = [
     (1, 1),
 ];
 
+fn parse_input() -> Vec<Vec<char>> {
+    let contents = fs::read_to_string("./inputs/day3.txt").expect("No file found");
+    let lines = contents.split("\n");
+
+    return lines
+        .filter(|l| l.len() > 0)
+        .map(|l| l.chars().collect())
+        .collect();
+}
+
 fn is_symbol(char: char) -> bool {
     return !char.is_digit(10) && char != '.';
 }
 
+// If a part number is found at the coordinates, flood fills it with '.' and
+// returns it. Returns 0 if out of bounds or no part number was found
 fn flood(map: &mut Vec<Vec<char>>, y: i32, x: i32) -> u32 {
+    // Out of bounds check
     if y < 0 || x < 0 || y >= map.len() as i32 || x >= map[y as usize].len() as i32 {
         return 0;
     }
+
     if map[y as usize][x as usize].is_digit(10) {
         let mut x_start = x;
         while x_start > 0 && map[y as usize][x_start as usize - 1].is_digit(10) {
             x_start -= 1;
         }
-        let mut num_str: String = String::new();
+        let mut part_number: String = String::new();
         while x_start < map[y as usize].len() as i32
             && map[y as usize][x_start as usize].is_digit(10)
         {
-            num_str.push(map[y as usize][x_start as usize]);
+            part_number.push(map[y as usize][x_start as usize]);
             map[y as usize][x_start as usize] = '.';
             x_start += 1;
         }
-        return num_str.parse().unwrap();
+        return part_number.parse().unwrap();
     }
+
     return 0;
 }
 
